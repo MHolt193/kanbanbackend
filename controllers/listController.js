f3dc9db43dd69982879decb94d205c5e5151c0e9
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2022-06-28 20:59:12 -0600

    added ability to find specific item with get call

--- controllers/listController.js
+++ controllers/listController.js
@@ -1,22 +1,30 @@
 const List = require("../models/listModel");
 
 const getList = async (req, res) => {
-  const list = await List.find({ board: req.body.boardId });
+  const list = await List.find({ board: req.params.id });
+  if (list.length > 0) {
+    res.status(200).json(list);
+  } else {
+    res.status(404).json({ message: "No tasks found" });
+  }
+};
 
-  res.status(200).json(list);
+const getItem = async (req, res) => {
+  const item = await List.find({ board: req.params.boardId, _id: req.params.itemId });
+
+  res.status(200).json(item)
 };
 
 const setList = async (req, res) => {
   const item = await List.create({
-    board: req.body.boardId,
+    board: req.body.board,
     title: req.body.title,
     description: req.body.description,
     subtasks: req.body.subtasks,
     status: req.body.status,
-    
   });
 
   res.status(200).json(item);
 };
 
-module.exports = {setList, getList}
+module.exports = { setList, getList, getItem };
--- routes/listRoutes.js
+++ routes/listRoutes.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getList, setList } = require("../controllers/listController");
+const { getList, getItem, setList } = require("../controllers/listController");
 const { protect } = require("../middleware/authMiddleware");
 
 
-router.get("/", protect, getList);
+router.get("/:id", protect, getList);
+router.get('/:boardId/:itemId', protect, getItem )
 router.post("/", protect, setList);
 //router.put("/:id", protect, editList);
 //router.delete("/:id", protect, deleteList);
