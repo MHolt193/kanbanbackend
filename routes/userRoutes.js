af2f8b0a15c2973a038857f8ba7822ce70c3cf70
MHolt193 <76035004+MHolt193@users.noreply.github.com>
2023-07-08 13:50:27 -0600

    added POST route to reply to notifications

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {registerUser, loginUser, getUser, searchUser, inviteUser, getNotifications} = require('../controllers/userController')
+const {registerUser, loginUser, getUser, searchUser, inviteUser, getNotifications, updateNotifications} = require('../controllers/userController')
 const {protect} = require('../middleware/authMiddleware')
 
 router.post('/', registerUser)
@@ -9,5 +9,6 @@ router.post('/search', protect,searchUser)
 router.post('/invite',protect, inviteUser)
 router.get('/me', protect, getUser)
 router.get('/:id/notifications', protect, getNotifications)
+router.post('/:id/notifications', protect, updateNotifications)
 
 module.exports = router
